6693934aaabf767ddc41352a72c72e56bc9966f5
krystianagadaka <krystianagadaka@o2.pl>
2020-10-26 12:06:31 +0100

    dodanie nowego pliku do zadan nr1

---
+++ PSI1.py
@@ -0,0 +1,11 @@
+#1
+tekst="Czym jest Lorem Impsum"
+print(tekst)
+#2
+imie="Krystian"
+nazwisko="Nagadowski"
+liczba_liter1=imie.count(imie[1])
+liczba_liter2=nazwisko.count(nazwisko[2])
+print(f"W tekście jest {liczba_liter1} liter oraz {liczba_liter2}")
+#3
+
